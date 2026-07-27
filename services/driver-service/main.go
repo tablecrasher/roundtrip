25e156a690680b7ecc5a65f65593ca496f492a2a
Benjamin Khong <benkhong27@gmail.com>
2026-07-26 23:12:17 -0700

    feat: finding a suitable driver

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -47,7 +47,7 @@ func main() {
 	grpcServer := grpcserver.NewServer()
 	NewGrpcHandler(grpcServer, svc)
 
-	consumer := NewTripConsumer(rabbitmq)
+	consumer := NewTripConsumer(rabbitmq, svc)
 	go func() {
 		if err := consumer.Listen(); err != nil {
 			log.Fatalf("Failed to listen to the message: %v", err)
