8feaeb6e77a8c353f2c90f78776963f52922e0d3
Benjamin Khong <benkhong27@gmail.com>
2026-07-27 23:08:24 -0700

    feat: async rabbitmq tracing

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"roundtrip/shared/contracts"
+	"roundtrip/shared/tracing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -77,24 +78,28 @@ func (r *RabbitMQ) ConsumeMessages(queueName string, handler MessageHandler) err
 
 	go func() {
 		for msg := range msgs {
-			log.Printf("Received a message: %s", msg.Body)
+			if err := tracing.TracedConsumer(msg, func(ctx context.Context, d amqp.Delivery) error {
+				log.Printf("Received a message: %s", msg.Body)
 
-			if err := handler(ctx, msg); err != nil {
-				log.Printf("ERROR: Failed to handle message: %v. Message body: %s", err, msg.Body)
+				if err := handler(ctx, msg); err != nil {
+					log.Printf("ERROR: Failed to handle message: %v. Message body: %s", err, msg.Body)
 
-				// Nack the message. Set requeue to false to avoid immediate redelivery loops.
-				// Consider a dead-letter exchange (DLQ) or a more sophisticated retry mechanism for production.
-				if nackErr := msg.Nack(false, false); nackErr != nil {
-					log.Printf("ERROR: Failed to Nack message: %v", nackErr)
+					// Nack the message. Set requeue to false to avoid immediate redelivery loops.
+					// Consider a dead-letter exchange (DLQ) or a more sophisticated retry mechanism for production.
+					if nackErr := msg.Nack(false, false); nackErr != nil {
+						log.Printf("ERROR: Failed to Nack message: %v", nackErr)
+					}
+
+					return err
 				}
 
-				// Continue to the next message
-				continue
-			}
-
-			// Only Ack if the handler succeeds
-			if ackErr := msg.Ack(false); ackErr != nil {
-				log.Printf("ERROR: Failed to Ack message: %v. Message body: %s", ackErr, msg.Body)
+				// Only Ack if the handler succeeds
+				if ackErr := msg.Ack(false); ackErr != nil {
+					log.Printf("ERROR: Failed to Ack message: %v. Message body: %s", ackErr, msg.Body)
+				}
+				return nil
+			}); err != nil {
+				log.Printf("Error processing message: %v", err)
 			}
 		}
 	}()
@@ -110,16 +115,24 @@ func (r *RabbitMQ) PublishMessage(ctx context.Context, routingKey string, messag
 		return fmt.Errorf("failed to marshal message: %v", err)
 	}
 
+	msg := amqp.Publishing{
+		DeliveryMode: amqp.Persistent,
+		ContentType:  "application/json",
+		Body:         jsonMsg,
+	}
+
+	return tracing.TracedPublisher(ctx, TripExchange, routingKey, msg, r.publish)
+}
+
+func (r *RabbitMQ) publish(ctx context.Context, exchange, routingKey string, msg amqp.Publishing) error {
+
 	return r.Channel.PublishWithContext(ctx,
-		TripExchange, // exchange
-		routingKey,   // routing key
-		false,        // mandatory
-		false,        // immediate
-		amqp.Publishing{
-			ContentType:  "text/plain",
-			Body:         jsonMsg,
-			DeliveryMode: amqp.Persistent,
-		})
+		exchange,   // exchange
+		routingKey, // routing key
+		false,      // mandatory
+		false,      // immediate
+		msg,
+	)
 }
 
 func (r *RabbitMQ) setupExchangesAndQueues() error {
