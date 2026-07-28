42d4708222148236f4f46d306ef9124646e6c6a0
Benjamin Khong <benkhong27@gmail.com>
2026-07-27 21:57:58 -0700

    feat: payment events and consumer

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -178,6 +178,22 @@ func (r *RabbitMQ) setupExchangesAndQueues() error {
 		return err
 	}
 
+	if err := r.declareAndBindQueue(
+		PaymentTripResponseQueue,
+		[]string{contracts.PaymentCmdCreateSession},
+		TripExchange,
+	); err != nil {
+		return err
+	}
+
+	if err := r.declareAndBindQueue(
+		NotifyPaymentSessionCreatedQueue,
+		[]string{contracts.PaymentEventSessionCreated},
+		TripExchange,
+	); err != nil {
+		return err
+	}
+
 	return nil
 }
 
